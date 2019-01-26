a6a65dabdb499e48f8250027032877ffdfd60bcb
Tammy Metz <tammy@tammymetz.com>
2019-01-25 19:04:11 -0500

    add comment about encoding data as bytes object

--- tcp-client-py3.py
+++ tcp-client-py3.py
@@ -9,6 +9,9 @@ client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 # connect the client
 client.connect((target_host, target_port))
 
+# For Python 3, the data must be sent as a bytes object, not a string.
+# So we have to use .encode() and .decode() to convert back and forth.
+# https://stackoverflow.com/questions/33003498/typeerror-a-bytes-like-object-is-required-not-str
 # send data
 string_to_send = "GET / HTTP/1.1\r\nHost: google.com\r\n\r\n"
 client.send(string_to_send.encode())
